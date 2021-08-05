05c19deb39261cc89c2b9c88fba3ad87cf94b60f
jallend1 <jallend1@gmail.com>
2021-08-05 12:35:29 -0700

    Adds styling to NavBar active item and imports useRouter from Next.js

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,26 +1,30 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../styles/NavBar.module.css';
 const NavBar = () => {
+  const router = useRouter();
   return (
     <nav>
       <p className={styles.logo}>Jason D. Allen</p>
       <div>
         <ul className={styles.menu}>
-          <Link href='/' passHref>
-            <li>
+          <li className={router.pathname === '/' ? styles.active : null}>
+            <Link href='/' passHref>
               <a>Home</a>
-            </li>
-          </Link>
-          <Link href='/projects' passHref>
-            <li>
+            </Link>
+          </li>
+          <li
+            className={router.pathname === '/projects' ? styles.active : null}
+          >
+            <Link href='/projects' passHref>
               <a>Projects</a>
-            </li>
-          </Link>
-          <Link href='/about' passHref>
-            <li>
+            </Link>
+          </li>
+          <li className={router.pathname === '/about' ? styles.active : null}>
+            <Link href='/about' passHref>
               <a>About</a>
-            </li>
-          </Link>
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
