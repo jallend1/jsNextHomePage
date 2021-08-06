2258759c76cbcf4fdf2abf782367e5d75dfb901c
jallend1 <jallend1@gmail.com>
2021-08-05 19:08:05 -0700

    Adjusts image size on About page and removes CodeBlock component

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,6 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import CodeBlock from '../components/CodeBlock';
-import styles from '../styles/About.module.css';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/About.module.css";
 
 const About = () => {
   return (
@@ -9,19 +8,18 @@ const About = () => {
       <Head>
         <title>Jason D. Allen | About</title>
       </Head>
-      <div className='container'>
+      <div className="container">
         <main className={styles.about}>
           <div>
             <h2>Hello, there.</h2>
             <h1>I&apos;m Jason.</h1>
-            <CodeBlock />
           </div>
           <div>
             <Image
-              src='/images/Coffee.png'
-              alt='Jason'
-              width={600}
-              height={720.24}
+              src="/images/Coffee.png"
+              alt="Jason"
+              width={333}
+              height={400}
             />
           </div>
         </main>
