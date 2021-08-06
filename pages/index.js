652b78e163a01f9db8d48d4c38e9feda11819b47
jallend1 <jallend1@gmail.com>
2021-08-06 16:16:34 -0700

    Begins reworking primary page to display photo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
+import Image from 'next/image';
 import styles from "../styles/Home.module.css";
 import CodeBlock from "../components/CodeBlock";
+import profilePic from '../public/images/Coffee.png';
 
 export default function Home() {
   return (
@@ -11,13 +13,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <div>
+            <Image
+              src={profilePic}
+              alt="Jason"      
+            />
+          </div>
+        <div className={styles.welcome}>
         <div className={styles.bio}>
           <CodeBlock />
           <div className={styles.contact}>
             <button className={styles.btn}>Get in touch.</button>
           </div>
         </div>
-        <div className={styles.welcome}>
           <h2 className={styles.subtitle}>&lt;coder&gt;</h2>
           <h1>
             Using elegant code to craft
